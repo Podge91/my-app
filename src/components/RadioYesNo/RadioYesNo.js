b0c4a83b3d76b27e111245e3ca4bd462431a3cbd
Patrick Wilson <pwilson39@qub.ac.uk>
2021-11-02 11:23:28 +0000

    Added links to navbar

---
+++ src/components/RadioYesNo/RadioYesNo.js
@@ -0,0 +1,18 @@
+import Button from '@restart/ui/esm/Button';
+import React from 'react';
+import { ButtonGroup } from 'react-bootstrap';
+
+import './RadioYesNo.css';
+
+const RadioYesNo = () => (
+  <div className="RadioYesNo">
+    <ButtonGroup>
+      <Button>
+        <Input
+      </Button>
+    </ButtonGroup>
+  </div>
+);
+
+
+export default RadioYesNo;
